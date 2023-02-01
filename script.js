9e5b70a2b6fe84d7bffe8c001fc22e0da220efe1
Brent Bartlett <brent.bartlett1@gmail.com>
2023-02-01 15:14:57 -0800

    test animation works

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
   for (let i = 0; i < divs.length; i += 1) {
     divs[i].addEventListener("click", function(e) {
       e.target.classList.toggle("img-border");
-      console.log("click");
     });
   }
 })();
