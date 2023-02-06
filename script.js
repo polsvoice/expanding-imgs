96077f2ae4313609a23138b22742d95af7173df8
Brent Bartlett <brent.bartlett1@gmail.com>
2023-02-06 12:46:35 -0800

    expand only one box at a time

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 (function() {
-  const divs = document.querySelectorAll(".img-div");
-
-  for (let i = 0; i < divs.length; i += 1) {
-    divs[i].addEventListener("click", function(e) {
-      e.target.classList.toggle("img-widen");
-    });
-  }
+  const gallery = document.querySelector("#gallery");
+  let currentDiv;
+  gallery.addEventListener("click", function(e) {
+    if (currentDiv) {
+      currentDiv.classList.remove("img-widen");
+    }
+    currentDiv = e.target;
+    currentDiv.classList.add("img-widen");
+  });
 })();
